a8d7f25e57fd597e90312c12192c1ab661e14e88
trolerogame <danielgcampos.04@gmail.com>
2023-03-17 14:50:03 -0300

    change url of api

--- src/utils/fetchClime.js
+++ src/utils/fetchClime.js
@@ -1,4 +1,5 @@
-export const fetchClime = async url => {
-    const data = await fetch(url)
+export const fetchClime = async (operation,q,confg = '') => {
+    const data = 
+        await fetch(`http://api.weatherapi.com/v1/${operation}.json?key=aa607b2c5d7b467185a144234231703&q=${q}${confg}`)
     return await data.json()
 }
