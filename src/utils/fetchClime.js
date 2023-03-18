aadfa69064a19497f0b72940a8a7b22773c49b79
trolerogame <danielgcampos.04@gmail.com>
2023-03-17 22:05:02 -0300

    chnage fetching http to https

--- src/utils/fetchClime.js
+++ src/utils/fetchClime.js
@@ -1,5 +1,5 @@
 export const fetchClime = async (operation,q,confg = '') => {
     const data = 
-        await fetch(`http://api.weatherapi.com/v1/${operation}.json?key=aa607b2c5d7b467185a144234231703&q=${q}${confg}`)
+        await fetch(`https://api.weatherapi.com/v1/${operation}.json?key=aa607b2c5d7b467185a144234231703&q=${q}${confg}`)
     return await data.json()
 }
